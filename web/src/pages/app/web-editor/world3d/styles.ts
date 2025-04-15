6a614e4596a55c9df2f1f2d8a47681c957caffb3
enix403 <fog.code000@gmail.com>
2025-04-15 23:55:29 +0500

    Remove clutter

--- web/src/pages/app/web-editor/world3d/styles.ts
+++
@@ -1,7 +0,0 @@
-export const COLOR_SKY = "#F6F6F6";
-
-// export const COLOR_FLOOR = 0xFAFAFA;
-// export const COLOR_FLOOR = 0x7cdfaa;
-export const COLOR_FLOOR = 0xbedab0;
-// export const COLOR_WALL = 0xfd7a5f;
-export const COLOR_WALL = 0x0ec57f;
